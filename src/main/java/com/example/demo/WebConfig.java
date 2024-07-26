b87064bbe7a13d3803c784582000bb9c6c3e7c55
nelo92 <manuel.augusto.92@gmail.com>
2024-07-26 12:56:14 +0200

    feat: version handler

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,20 +1,34 @@
 package com.example.demo;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import io.undertow.servlet.api.DeploymentInfo;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Bean
-	public FilterRegistrationBean<CustomErrorFilter> urlDecodeFilter(){
-		FilterRegistrationBean<CustomErrorFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(new CustomErrorFilter());
-		registrationBean.addUrlPatterns("/*");
-		registrationBean.setOrder(Integer.MIN_VALUE);// Ensure this filter is invoked first
-		return registrationBean;
+	@Bean
+	public WebServerFactoryCustomizer<UndertowServletWebServerFactory> undertowServerFactoryCustomizer(){
+		return factory -> factory.addDeploymentInfoCustomizers(new UndertowDeploymentInfoCustomizer() {
+			@Override
+			public void customize(DeploymentInfo deploymentInfo) {
+				deploymentInfo.addInnerHandlerChainWrapper(handler -> new CustomHttHandler(handler));
+			}
+		});
 	}
+
+//		implements WebMvcConfigurer {
+//
+//    @Bean
+//	public FilterRegistrationBean<CustomErrorFilter> urlDecodeFilter(){
+//		FilterRegistrationBean<CustomErrorFilter> registrationBean = new FilterRegistrationBean<>();
+//		registrationBean.setFilter(new CustomErrorFilter());
+//		registrationBean.addUrlPatterns("/*");
+//		registrationBean.setOrder(Integer.MIN_VALUE);// Ensure this filter is invoked first
+//		return registrationBean;
+//	}
 
 }
